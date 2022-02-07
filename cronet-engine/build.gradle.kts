6f0404844b3d42fe72e68c59a48697d77c638ce5
Yuya Matsuo <niu@niusounds.com>
2022-02-08 00:38:16 +0900

    setup jitpack

--- cronet-engine/build.gradle.kts
+++ cronet-engine/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
     id("org.jetbrains.dokka")
 }
+
+group = "com.github.niusounds"
+version = "0.0.1"
 
 android {
     compileSdk = 31
@@ -36,4 +40,14 @@ android {
 dependencies {
     implementation("io.ktor:ktor-client-core:1.6.7")
     implementation("com.google.android.gms:play-services-cronet:18.0.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components.findByName("release"))
+            }
+        }
+    }
 }
