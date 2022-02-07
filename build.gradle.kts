3cdad4f5dd135680a0b64a7643c51422828935ea
Yuya Matsuo <niu@niusounds.com>
2022-02-07 22:14:59 +0900

    add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.dokka") version "1.6.10" apply false
 }
 
 tasks.create<Delete>("clean") {
--- cronet-engine/src/main/java/com/niusounds/ktor/client/engine/cronet/Cronet.kt
+++ cronet-engine/src/main/java/com/niusounds/ktor/client/engine/cronet/Cronet.kt
@@ -3,6 +3,10 @@ package com.niusounds.ktor.client.engine.cronet
 import io.ktor.client.*
 import io.ktor.client.engine.*
 
+/**
+ * [HttpClientEngineFactory] using a [CronetEngine] based backend implementation
+ * with the associated configuration [CronetConfig].
+ */
 object Cronet : HttpClientEngineFactory<CronetConfig> {
     override fun create(block: CronetConfig.() -> Unit): HttpClientEngine =
         CronetEngine(CronetConfig().apply(block))
